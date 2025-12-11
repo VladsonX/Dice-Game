9bb6eafe7e3a00f532841535eca8d8b643a08778
Uladzislau Navitski <vladnovik123456789@gmail.com>
2025-12-11 17:49:35 +0100

    feature: design is chanhed, scoreboard added

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const scoreboard = document.querySelector("#scoreboard");
 const player1Label = document.querySelector("#player1Label");
 const player2Label = document.querySelector("#player2Label");
 const logInForm = document.querySelector("#logIn");
+const nickNames = document.querySelectorAll(".nickname");
 
 logInForm.addEventListener("submit", startGame);
 
@@ -30,6 +31,8 @@ function startGame(event) {
   console.log("Game started");
   player1Label.textContent = player1Name;
   player2Label.textContent = player2Name;
+  document.querySelector("#scoreboardPlayer1").textContent = player1Name;
+  document.querySelector("#scoreboardPlayer2").textContent = player2Name;
 }
 
 function finishGame() {
@@ -50,9 +53,28 @@ function playGame() {
 
   const dice1 = document.querySelector("#dice1");
   const dice2 = document.querySelector("#dice2");
+  const scorePlayer1 = document.querySelector("#scorePlayer1");
+  const scorePlayer2 = document.querySelector("#scorePlayer2");
 
+  dice1.classList.remove("dice_winner");
+  dice2.classList.remove("dice_winner");
+  nickNames[0].classList.remove("player_winner");
+  nickNames[1].classList.remove("player_winner");
   renderDice(dice1, dice1Number);
   renderDice(dice2, dice2Number);
+  setTimeout(() => {
+    if (dice1Number > dice2Number) {
+      winDice(dice1, nickNames[0]);
+      scorePlayer1.textContent = Number(scorePlayer1.textContent) + 1;
+    } else if (dice1Number < dice2Number) {
+      winDice(dice2, nickNames[1]);
+      scorePlayer2.textContent = Number(scorePlayer2.textContent) + 1;
+    } else {
+      draw(dice1, dice2, nickNames[0], nickNames[1]);
+      scorePlayer1.textContent = Number(scorePlayer1.textContent) + 1;
+      scorePlayer2.textContent = Number(scorePlayer2.textContent) + 1;
+    }
+  }, 300);
 }
 
 function renderDice(dice, numberOfDots) {
@@ -60,6 +82,7 @@ function renderDice(dice, numberOfDots) {
   const diceDots = dice.querySelectorAll(".dice__dot");
   diceRows.forEach((e) => e.classList.remove("center", "end"));
   diceDots.forEach((e) => e.classList.remove("display"));
+  dice.classList.toggle("rotate");
   switch (numberOfDots) {
     case 0: //1
       diceRows[1].classList.add("center");
@@ -108,12 +131,23 @@ function renderDice(dice, numberOfDots) {
   }
 }
 
+function winDice(dice, player) {
+  dice.classList.add("dice_winner");
+  player.classList.add("player_winner");
+}
+
+function draw(dice1, dice2, player1, player2) {
+  dice1.classList.add("dice_winner");
+  player1.classList.add("player_winner");
+  dice2.classList.add("dice_winner");
+  player2.classList.add("player_winner");
+}
 
 const player1DynamicName = document.querySelector("#player1DynamicName");
 const player2DynamicName = document.querySelector("#player2DynamicName");
 player1Name.addEventListener("input", (e) => {
-    player1DynamicName.textContent = e.target.value;
-})
+  player1DynamicName.textContent = e.target.value;
+});
 player2Name.addEventListener("input", (e) => {
-    player2DynamicName.textContent = e.target.value;
-})
+  player2DynamicName.textContent = e.target.value;
+});
